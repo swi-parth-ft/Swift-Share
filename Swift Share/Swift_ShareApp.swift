7b54595e1bbe18be56ba094abe156deeff6ba0bf
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2023-03-13 12:21:08 -0400

    Initial Commit

---
+++ Swift Share/Swift_ShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_ShareApp.swift
+//  Swift Share
+//
+//  Created by Parth Antala on 2023-03-13.
+//
+
+import SwiftUI
+
+@main
+struct Swift_ShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
